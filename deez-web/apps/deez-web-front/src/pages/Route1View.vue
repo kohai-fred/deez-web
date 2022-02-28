0e0e1bb6aa3a31ccb9de8c4190d78374c41f42a1
kohai-fred <frederick.vu.pro@gmail.com>
2022-03-01 00:36:25 +0100

    feat: use proxy for fetch

---
+++ deez-web/apps/deez-web-front/src/pages/Route1View.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { onMounted } from "vue";
+    console.log("route_1");
+    onMounted(async () => {
+        const response = await fetch("http://localhost:8080/playlists").then(
+            (res) => res.json()
+        );
+        console.log("reponse", response);
+    });
+</script>
+
+<template>
+    <h1>ROUTE 1</h1>
+</template>
--- deez-web/src/pages/Route1View.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>ROUTE 1</h1>
-</template>
